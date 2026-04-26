c4b00f9d8af36df2b0542a7d1d5776b88d90789f
tgorman <t.gorman51@gmail.com>
2026-04-26 15:04:33 -0400

    render content, then render base template

--- build.js
+++ build.js
@@ -1,16 +1,19 @@
 const fs = require("fs");
 
 const config = {
-    templatePath: "templates/"
+    templatePath: "templates/",
+    baseTemplateName: "base-html-template"
 };
 
 const pages = {
     home: {
         title: "Home",
+        templateName: "generic-page",
         content: "Hello world"
     },
     about: {
         title: "About",
+        templateName: "generic-page",
         content: "This is the about page. Here is some filler text to make this content long enough for testing a whole paragraph of text. While I have you here, have you ever considered that it's technically physically possible for an entire lobster to instantaneously be transported to the moon? That's true! I won't explain any more."
     }
 };
@@ -26,11 +29,12 @@ function renderTemplate(template, data) {
 function render(pageName) {
     const page = pages[pageName];
     
-    // Load the HTML template
-    const rawTemplate = loadTemplate("generic-page");
-
-    // Return the template populated with page data/content
-    return renderTemplate(rawTemplate, page);
+    // Load and render the HTML template
+    const rawTemplate = loadTemplate(page["templateName"]);
+    page["renderedContent"] = renderTemplate(rawTemplate, page);
+    
+    return renderTemplate(loadTemplate(config.baseTemplateName), page);
 }
 
-fs.writeFileSync("dist/index.html", render("about"));
+fs.writeFileSync("dist/index.html", render("home"));
+fs.copyFileSync("styles.css", "dist/styles.css");
